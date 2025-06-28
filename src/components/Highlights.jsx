357a157660afa488fff415d2be972b7e0aa98a31
Abdulazeez20012 <abdulazeezmuhammad855@gmail.com>
2025-06-28 18:32:06 +0100

    working on the hero section and also currently working on the highlights section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,11 @@
 const Highlights = () => {
   return (
-    <div>Highlights</div>
+    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-Zinc">
+        <div>
+            <h1 id="title" className="section-heading">Get the Highlights.</h1> <h1 id="subtitle" className="section-heading" ></h1>
+        </div>
+
+    </section>
   )
 }
 
